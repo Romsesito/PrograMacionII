b4e443306c6dbe2babb8672e6fbfe33d9443c7a5
Romsesito <105876509+Romsesito@users.noreply.github.com>
2023-01-18 13:57:23 -0500

    FIX

--- PacoLO/PACOGERTE.C
+++ PacoLO/PACOGERTE.C
@@ -3,21 +3,23 @@
 int main(){
 
     FILE *vector=NULL;
-    char buffer[100];
-    double pi;
-
+    FILE *vector2=NULL;
     vector=fopen("vector.txt","r+");
-    vector=fopen("vector1.txt","w");
-
-    if (vector==NULL){
+    vector2=fopen("vectormult.txt","w");
+    int a,b;
+    
+    if (vector==NULL)
+    {
         printf("No se puede abrir el archivo");
         return -1;
     }
 
-  int paco=3;
     for(int i=0; i<100; i++){
-        fprintf(vector,"%d\n", i * paco);
-    }
-     fclose(vector);
-     return 0;
-}
+        fscanf(vector,"%d",&a);
+        b=a*3;
+        fprintf(vector2,"%d\n",b);
+        }
+        fclose(vector);
+        fclose(vector2);
+        return 0;
+     }
